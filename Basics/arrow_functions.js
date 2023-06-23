85700586596e7452785037b95700cb95ace44329
Walé <56082759+WaleAwo@users.noreply.github.com>
2023-06-23 09:22:53 +0100

    added arrow function example

---
+++ Basics/arrow_functions.js
@@ -0,0 +1,17 @@
+// simplified way of writing a function
+
+// before arrow
+hello = function () {
+  return "Hello World";
+};
+
+// with arrow function
+hello = () => {
+  return "Hello World!";
+};
+
+// a function with one return statement
+hello = () => "Hello World!";
+
+// a function that takes a parameter
+hello = (val) => "Hello " + val;
