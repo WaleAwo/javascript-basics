6b183855835a1121c031347aa19be0fd90261d7f
Wale <56082759+WaleAwo@users.noreply.github.com>
2024-08-12 21:06:59 +0100

    added comments

--- Tasks/checkAlphabeticalOrder.js
+++ Tasks/checkAlphabeticalOrder.js
@@ -2,7 +2,7 @@ let phrase = "CBA";
 let stringLength = phrase.length;
 
 for (let i = 0; i < stringLength - 1; i++) { // -1 to not check the last character 
-  if (phrase[i] > phrase[i + 1]) {
+  if (phrase[i] > phrase[i + 1]) { // using ASCI numbering
     console.log("Not in alphabetical order");
     return 0;
   }
